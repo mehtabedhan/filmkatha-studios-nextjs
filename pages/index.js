8f4e8a3ef2a0b8609d2b9ae50ea47ff66e80d7d0
Mehtab Singh Edhan <37650533+mehtabedhan@users.noreply.github.com>
2023-04-01 15:02:57 +0530

    go

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import { FilmCategoriesSection, FilmsSection, UpcomingFilmsSection } from '../components';
 import Footer from '../components/Footer';
 import VideoPlayer from '../components/VideoPlayer';
+import { aboutUs } from '../data';
 
 export default function Home() {
   return (
     <div className="">
+       <Head>
+        <title>Filmkatha Studios</title>
+        <meta charset="UTF-8"/>
+
+        <meta
+          name="description"
+          content={aboutUs}
+          key="desc"
+        />
+        <meta name="keywords"
+         content={'filmkatha,filmkatha studios'}
+         />
+
+        <link
+          rel="canonical"
+          href={domainUrl}
+          key="canonical"
+        />
+    </Head>
 
       <FilmsSection/>
       <UpcomingFilmsSection/>
